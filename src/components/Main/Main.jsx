af0349d128a59322501aa162e5c23c2a5719ae33
Alexander Sturm <sturmgegencoding@gmail.com>
2024-08-03 21:16:40 -0500

    constants are components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import "./Main.css";
 import "./WeatherCard/WeatherCard";
 import WeatherCard from "./WeatherCard/WeatherCard";
-import { defaultClothingItems } from "../../utils/Constants";
+import { DefaultClothingItems } from "../../utils/Constants";
 import { ItemCard } from "./ItemCard/ItemCard";
 import { Footer } from "../Footer/Footer";
 import { ModalWithForm } from "../ModalWithForm/ModalWithForm";
@@ -15,19 +15,17 @@ export default function Main({ weatherData, handleCardClick }) {
           Today is {weatherData.temp.F} &deg; F / You may want to wear:
         </p>
         <ul className="cards__list">
-          {defaultClothingItems
-            .filter((item) => {
-              return item.weather === weatherData.type;
-            })
-            .map((item) => {
-              return (
-                <ItemCard
-                  key={item._id}
-                  item={item}
-                  onCardClick={handleCardClick}
-                />
-              );
-            })}
+          {DefaultClothingItems.filter((item) => {
+            return item.weather === weatherData.type;
+          }).map((item) => {
+            return (
+              <ItemCard
+                key={item._id}
+                item={item}
+                onCardClick={handleCardClick}
+              />
+            );
+          })}
         </ul>
       </section>
       <Footer />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const defaultClothingItems = [
+export const DefaultClothingItems = [
   {
     _id: 0,
     name: "Cap",
